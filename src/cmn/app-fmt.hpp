e3d49e4c76cc983c92807aa739a0fec61c269b00
cwood77 <dumuzi77@gmail.com>
2022-04-10 15:08:07 +0100

    dum pragma pack... :(

--- src/cmn/app-fmt.hpp
+++ src/cmn/app-fmt.hpp
@@ -4,6 +4,10 @@
 namespace cmn {
 namespace appfmt {
 
+// TODO pragma pack these chumps or they will create unused garbage in the files
+//      that shouldn't caue a problem, per se, but will inflate things on disk
+//      and introduce an source of randomness in build output
+
 class header {
 public:
    header() : version(0), tocEntryCnt(0)
