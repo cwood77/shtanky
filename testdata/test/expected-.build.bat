e650978eafaa32d0dd922d349a42cd7ec69d3d5a
cwood77 <dumuzi77@gmail.com>
2022-04-15 16:23:47 +0100

    update expected test output

--- testdata/test/expected-.build.bat
+++ testdata/test/expected-.build.bat
@@ -4,16 +4,16 @@ echo *****************
 echo ***   liam    ***
 echo *****************
 bin\out\debug\liam ".\testdata\test\test.ara.ls"
-bin\out\debug\liam "testdata\sht\cons\program.ara.ls"
+bin\out\debug\liam ".\testdata\sht\cons\program.ara.ls"
 
 echo *******************
 echo ***   shtasm    ***
 echo *******************
 bin\out\debug\shtasm "testdata\sht\oscall.asm"
 bin\out\debug\shtasm ".\testdata\test\test.ara.ls.asm"
-bin\out\debug\shtasm "testdata\sht\cons\program.ara.ls.asm"
+bin\out\debug\shtasm ".\testdata\sht\cons\program.ara.ls.asm"
 
 echo *******************
 echo ***   shlink    ***
 echo *******************
-bin\out\debug\shlink ".\testdata\test\.app" ".\testdata\test\test.ara.ls.asm.o" "testdata\sht\cons\program.ara.ls.asm.o" "testdata\sht\oscall.asm.o"
+bin\out\debug\shlink ".\testdata\test\.app" ".\testdata\test\test.ara.ls.asm.o" ".\testdata\sht\cons\program.ara.ls.asm.o" "testdata\sht\oscall.asm.o"
