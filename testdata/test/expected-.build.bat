23e4bb85614072c8d040cbd4de3ffdf65fcac867
cwood77 <dumuzi77@gmail.com>
2022-04-16 21:47:15 +0100

    fix empty class parse and type prop for all built-ins

--- testdata/test/expected-.build.bat
+++ testdata/test/expected-.build.bat
@@ -4,6 +4,7 @@ echo *****************
 echo ***   liam    ***
 echo *****************
 bin\out\debug\liam ".\testdata\test\test.ara.ls"
+bin\out\debug\liam ".\testdata\test\.target.ara.ls"
 bin\out\debug\liam ".\testdata\sht\cons\program.ara.ls"
 
 echo *******************
@@ -11,9 +12,10 @@ echo ***   shtasm    ***
 echo *******************
 bin\out\debug\shtasm "testdata\sht\oscall.asm"
 bin\out\debug\shtasm ".\testdata\test\test.ara.ls.asm"
+bin\out\debug\shtasm ".\testdata\test\.target.ara.ls.asm"
 bin\out\debug\shtasm ".\testdata\sht\cons\program.ara.ls.asm"
 
 echo *******************
 echo ***   shlink    ***
 echo *******************
-bin\out\debug\shlink ".\testdata\test\.app" ".\testdata\test\test.ara.ls.asm.o" ".\testdata\sht\cons\program.ara.ls.asm.o" "testdata\sht\oscall.asm.o"
+bin\out\debug\shlink ".\testdata\test\.app" ".\testdata\test\test.ara.ls.asm.o" ".\testdata\test\.target.ara.ls.asm.o" ".\testdata\sht\cons\program.ara.ls.asm.o" "testdata\sht\oscall.asm.o"
