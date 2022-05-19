4779317c4ef8a3713bc14cd889e9cff9ac26e181
cwood77 <dumuzi77@gmail.com>
2022-05-19 14:51:48 +0100

    rule out the ptr solution for the LEA problem

--- testdata/shtemu/basic.asm
+++ testdata/shtemu/basic.asm
@@ -1,6 +1,9 @@
 .seg data
 helloWorld: .data, "hello world" <b> 0
 
+;.seg data
+;helloWorldPtr: .data, helloWorld
+
 .seg code
 .entrypoint:
    push, rbp
@@ -8,7 +11,14 @@ helloWorld: .data, "hello world" <b> 0
    sub, rsp, 32
 
    mov, rcx, 1
+   ; there is no way around this LEA
+   ; at first, I thought I could generate a second ptr
+   ; allocation in the datasegment that could patch, and then
+   ; reference that with a MOV.  That doesn't work, because
+   ; the address in the ptr allocation is still patched at link-time,
+   ; not load-time, so it's still wrong at run-time.
    lea, rdx, qwordptr helloWorld
+;  mov, rdx, qwordptr helloWorldPtr
    call, ._osCall
 
 ;   add, rsp, 32 ; add is not sufficient
